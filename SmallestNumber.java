c1ab0502c12ad4a71ebb7c6e167877de9cf69662
DevainSikka <devain97@gmail.com>
2023-10-03 09:39:39 +0530

    Update SmallestNumber.java

--- SmallestNumber.java
+++ SmallestNumber.java
@@ -7,39 +7,17 @@ public class SmallestNumber{
             System.out.println("enter the number ");
             num[i] = sc.nextDouble();
         } 
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j <3; j++){
+                if(num[i] < num[j]){
+                    double temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+            }
+        }
+    }
         double smallest = num[0];
-                                                        
-    if (num[1] < smallest) {
-        smallest = num[1];
-    }
-
-    if (num[2] < smallest) {
-        smallest = num[2];
-     }
-        System.out.println("smallest number is " + smallest);
-    if(num[0] == smallest){
-        if(num[1]<num[2]){
-            System.out.println(num[0]+"<"+num[1]+"<"+num[2]);
-        }
-        if(num[2]<num[1]){
-            System.out.println(num[0]+"<"+num[2]+"<"+num[1]);
-        }
-    }
-    if(num[1] == smallest){
-        if(num[0]<num[2]){
-            System.out.println(num[1]+"<"+num[0]+"<"+num[2]);
-        }
-        if(num[2]<num[0]){
-            System.out.println(num[1]+"<"+num[2]+"<"+num[0]);
-        }
-    }
-    if(num[2] == smallest){
-        if(num[1]<num[0]){
-            System.out.println(num[2]+"<"+num[1]+"<"+num[0]);
-        }
-        if(num[0]<num[1]){
-            System.out.println(num[2]+"<"+num[0]+"<"+num[1]);
-        }
-    }
+        System.out.println("smallest number is" + num[0]);
+        System.out.println("final series is"+ num[0]+","+num[1]+","+num[2]);
     }
 }
